03f2af565f6d5db03f7112ffef93dd4dd520e55f
Iliyas Ahad <81019531+iliyasahad@users.noreply.github.com>
2024-12-19 09:26:53 +0000

    Removed playGame & playRound functions
    Will rewrite these functions

--- javascript.js
+++ javascript.js
@@ -23,38 +23,4 @@ function getHumanChoice() {
 }
 
 
-function playGame() {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice(); 
 
-    function playRound(humanChoice, computerChoice) {
-        if ((humanChoice === "rock" && computerChoice === "rock") || 
-        (humanChoice === "paper" && computerChoice === "paper") || 
-        (humanChoice === "scissors" && computerChoice === "scissors")) {
-            return "Tie";
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            return humanScore++;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            return computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            return humanScore++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            return computerScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerChoice++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            return humanScore++;
-        }
-        return 1;
-    }
-
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-
-    return 0;
-}
-
-playGame();
